a7c8590b61e217108ffa78d2d700b626c955a334
IpinBoys <m.ainularifin@gmail.com>
2017-11-24 22:21:39 +0700

    Menambahkan portal sekolah untuk kepentingan pembelajaran sekolah

--- sekolah/application/config/webmaster.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['apps']      = 'Free CMS Sekolahku';
-$config['version']   = '2.0.4';
-$config['webmaster'] = 'Anton Sofyan';
-$config['email']     = '4ntonsofyan@gmail.com';
-$config['website']   = 'http://www.sekolahku.web.id';
